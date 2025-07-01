9141c81798127973698d8807714677ecc127acda
yashsavaliya951 <savaliyayash161@gmail.com>
2025-07-01 23:19:15 +0530

    dataTypes and variables

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId= 23654;
+let accountEmail =  "yash@gmail.com";
+var accountPass = 232323;
+let accountState;
+
+
+
+accountEmail = "yash@google.com";
+accountPass = 565656;
+
+console.table([accountId,accountEmail,accountPass,accountState])
---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,11 @@
+let number = 20;   //number 2 to power 53
+let name = "yash"; //String
+let isMulti = true; //boolean
+let abc = null;     //object
+let udfin;          //undefine
+
+// this both also data-type
+//symbol
+//object
+console.log(typeof null);
+console.log(typeof udfin);
